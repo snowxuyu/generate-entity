2a0bf4ba33bf62f322a7fa2659a65fa04534d391
gaoguoxiang <snowxuyu@gmail.com>
2016-08-11 19:28:13 +0800

    np update

---
+++ src/main/java/org/snow/autogen/AutoGeneratorTest.java
@@ -0,0 +1,13 @@
+package org.snow.autogen;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: snowxuyu
+ * Date: 2016/8/4
+ * Time: 21:41
+ */
+public class AutoGeneratorTest {
+    public static void main(String[] args){
+        AutoGenerate.getInstanceInit();
+    }
+}
